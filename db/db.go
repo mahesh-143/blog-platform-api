8d2b3dae6e1a97a95edadbdaf083dabd4aa94cb5
mahesh-143 <maheshodedara13@yahoo.com>
2024-07-11 21:17:43 +0530

    add environment variables

--- db/db.go
+++ db/db.go
@@ -4,27 +4,31 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var Connection *sql.DB
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "mahesh"
-	password = "password"
-	dbname   = "blog"
-)
-
 func InitDB() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading .env file")
+	}
+
+	host := os.Getenv("HOST")
+	port := 5432
+	user := "mahesh"
+	password := "password"
+	dbname := "blog"
 
 	connectionStr := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	var err error
 	Connection, err = sql.Open("postgres", connectionStr)
 	if err != nil {
 		log.Fatal("error validating database connection : ", err)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mahesh-143/blog-platform-api
 
 go 1.22.1
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
